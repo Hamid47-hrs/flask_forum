2e110c56b40bc4429f002cff52a22a5b655e5020
Hamid47 <hamid47.hrs@gmail.com>
2023-03-06 21:58:14 +0330

    Initialize the database with 'SQLAlchemy'.

--- forum/app/__init__.py
+++ forum/app/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from app.users.routes import blueprint as users_blueprint
 from app.posts.routes import blueprint as posts_blueprint
 
 app = Flask(__name__)
 
 # ** For Development insert 'config.DevelopmentConfig' and for Production insert 'config.ProductionConfig'.
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object("config.DevelopmentConfig")
 
 app.register_blueprint(users_blueprint)
 app.register_blueprint(posts_blueprint)
+
+
+db = SQLAlchemy(app)
+
+# ! Use app_context() to create the application before initializing the database.
+app.app_context().push()
+
+
+db.create_all()
